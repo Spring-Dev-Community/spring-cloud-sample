d43ac4b8a342b4e278ebdd2caa1ac64b39c5f790
ReLive27 <re_live27@163.com>
2023-08-15 22:25:24 +0800

    Enable dynamic-config module

---
+++ spring-cloud-consul/dynamic-config/src/main/java/com/relive27/ConsulDynamicConfigApplication.java
@@ -0,0 +1,16 @@
+package com.relive27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive27
+ * @date: 2023/8/14 19:44
+ */
+@SpringBootApplication
+public class ConsulDynamicConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulDynamicConfigApplication.class, args);
+    }
+}
