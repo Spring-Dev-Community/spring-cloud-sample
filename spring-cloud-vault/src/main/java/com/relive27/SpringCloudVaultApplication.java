2ee0502874a01930418f0cfaf684c5093056a3a3
ReLive27 <re_live27@163.com>
2024-04-19 08:06:40 +0800

    Init spring-cloud-vault

---
+++ spring-cloud-vault/src/main/java/com/relive27/SpringCloudVaultApplication.java
@@ -0,0 +1,16 @@
+package com.relive27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive27
+ * @date: 2022/12/14 22:04 下午
+ */
+@SpringBootApplication
+public class SpringCloudVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudVaultApplication.class, args);
+    }
+}
